ca988d0eaff84848830022bf20bcbbae82a6d3e8
Jose Gil <u20251b192@upc.edu.pe>
2025-06-13 11:24:19 -0500

    Primer commit

---
+++ variables/variables.py
@@ -0,0 +1,20 @@
+
+#Definiendo una variable con camelCase 
+nombreCompletoDeTuTioMaster = "Lucas Dalto"
+
+#Definiendo una variable con snake_case
+nombre_completo_de_tu_tio_master = "Lucas Dalto"
+
+
+#concatenar con +
+bienvenida = "Hola " + " ¿Como estas?"
+
+#concatenar con f-strings
+bienvenida = f"Hola {nombre} ¿Como estas?"
+
+
+#operadores de pertenencia (in / not in) 55:49
+print("Lucas" in bienvenida) #true
+print("Lucas" not in bienvenida) #false
+
+
